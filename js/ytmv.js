6611f126ed6ce99cad1e78b0f435c10b80acf1df
James <james.peng.pkj@gmail.com>
2026-01-02 12:23:08 +0800

    make it better

---
+++ js/ytmv.js
@@ -0,0 +1,562 @@
+const songDB = [
+    {
+        "id": 1,
+        "artist": "周杰倫",
+        "title": "周杰倫Jay Chou X aMEI【不該 Shouldn't Be】Official MV",
+        "vid": "_VxLOj3TB5k",
+        "surname": "周"
+    },
+    {
+        "id": 2,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【手寫的從前 Handwritten Past】Official MV",
+        "vid": "TMB6-YflpA4",
+        "surname": "周"
+    },
+    {
+        "id": 3,
+        "artist": "周杰倫",
+        "title": "周杰倫 JAY CHOU & 五月天 MAYDAY【稻香 搖滾版】",
+        "vid": "4dIiSXT8oYk",
+        "surname": "周"
+    },
+    {
+        "id": 4,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【聽見下雨的聲音 Rhythm of the Rain】Official MV",
+        "vid": "zqKoXPHhmsM",
+        "surname": "周"
+    },
+    {
+        "id": 5,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【Mine Mine】Official MV",
+        "vid": "me7jfhFIZvk",
+        "surname": "周"
+    },
+    {
+        "id": 6,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【Mojito】Official MV ★ Check out \"J-Style Trip\" on Netflix -Travelogue, Magic and Fun!",
+        "vid": "-biOGdYiF-I",
+        "surname": "周"
+    },
+    {
+        "id": 7,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【等你下課 Waiting For You  (with 楊瑞代 Gary)】歌詞版Lyrics MV",
+        "vid": "kfXdP7nZIiE",
+        "surname": "周"
+    },
+    {
+        "id": 8,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【聽爸爸的話 Listen to Dad】Official MV",
+        "vid": "eS2T9IiOYbc",
+        "surname": "周"
+    },
+    {
+        "id": 9,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【明明就 Ming Ming Jiu】Official MV",
+        "vid": "UBySINroNkw",
+        "surname": "周"
+    },
+    {
+        "id": 10,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【暗號 Secret Code】Official MV",
+        "vid": "CYT9DPJdtS4",
+        "surname": "周"
+    },
+    {
+        "id": 11,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【爺爺泡的茶 Grandpa's Tea】Official MV",
+        "vid": "LdPjnubLRN0",
+        "surname": "周"
+    },
+    {
+        "id": 12,
+        "artist": "周杰倫",
+        "title": "【稻香 Rice Field】周杰倫 Jay Chou《歌詞》",
+        "vid": "IgpAUdCPKK8",
+        "surname": "周"
+    },
+    {
+        "id": 13,
+        "artist": "周杰倫",
+        "title": "周杰伦   发如雪 Karaoke",
+        "vid": "CsQBQB08JS4",
+        "surname": "周"
+    },
+    {
+        "id": 14,
+        "artist": "周杰倫",
+        "title": "純音樂 周杰倫 Jay Chou ─《不能說的秘密》Wild West KTV 伴唱 Karaoke 伴奏 西野",
+        "vid": "YGceKnbXWzQ",
+        "surname": "周"
+    },
+    {
+        "id": 15,
+        "artist": "周杰倫",
+        "title": "周杰倫 JAY CHOU【倒影 Reflection】Official MV",
+        "vid": "a0AyNzV3yk8",
+        "surname": "周"
+    },
+    {
+        "id": 16,
+        "artist": "周杰倫",
+        "title": "周杰倫 Jay Chou【錯過的煙火 You Are The Firework I Missed】Official MV",
+        "vid": "9tN1xixvYOc",
+        "surname": "周"
+    },
+    {
+        "id": 17,
+        "artist": "周杰倫",
+        "title": "【KTV】周杰伦 Jay Chou《简单爱》原版伴奏 | 高清歌词 (Karaoke Version)",
+        "vid": "wJt-B0hIcmg",
+        "surname": "周"
+    },
+    {
+        "id": 18,
+        "artist": "周杰倫",
+        "title": "【繁中】周杰倫-等你下課KTV(無人聲)",
+        "vid": "YL8Z78av-Bs",
+        "surname": "周"
+    },
+    {
+        "id": 19,
+        "artist": "周杰倫",
+        "title": "周杰倫 - 告白氣球 歌詞",
+        "vid": "G9UQ63tWIqA",
+        "surname": "周"
+    },
+    {
+        "id": 20,
+        "artist": "周杰倫",
+        "title": "蒲公英的约定 周杰伦 (歌词版)",
+        "vid": "SmaHHH_3Wq8",
+        "surname": "周"
+    },
+    {
+        "id": 21,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 過火 ] 官方完整版 Official MV",
+        "vid": "SrD36rogSjc",
+        "surname": "張"
+    },
+    {
+        "id": 22,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 太想愛你 ]  官方完整版 Official MV",
+        "vid": "hG4-xzaDv40",
+        "surname": "張"
+    },
+    {
+        "id": 23,
+        "artist": "張信哲",
+        "title": "【纯享】#张信哲 《愛如潮水 過火 別怕我傷心 信仰》经典串烧四连唱，好听到流泪！| 2016湖南卫视跨年演唱会 | Mango TV",
+        "vid": "N5UKJ3aZItk",
+        "surname": "張"
+    },
+    {
+        "id": 24,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 寬容 ] 官方完整版 MV",
+        "vid": "fQItfi8WsK8",
+        "surname": "張"
+    },
+    {
+        "id": 25,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 屬於我們的故事 ] 官方完整版 Official MV",
+        "vid": "BAUSVk-GcN4",
+        "surname": "張"
+    },
+    {
+        "id": 26,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 不要對他說 ] 官方完整版 Official MV",
+        "vid": "3J7rfx0NofU",
+        "surname": "張"
+    },
+    {
+        "id": 27,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 這世的名字 ] 官方完整版 Official MV",
+        "vid": "6LmFPk-FJQU",
+        "surname": "張"
+    },
+    {
+        "id": 28,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 用情 ] 官方完整版 MV",
+        "vid": "M5MliGcbqNM",
+        "surname": "張"
+    },
+    {
+        "id": 29,
+        "artist": "張信哲",
+        "title": "【纯享】#张信哲 深情演绎《信仰》醇厚嗓音穿透灵魂，用歌声诉说炽热且坚定的爱 | 我是歌手4 | I Am A Singer 4 | MangoTV",
+        "vid": "uRPo23_vnmc",
+        "surname": "張"
+    },
+    {
+        "id": 30,
+        "artist": "張信哲",
+        "title": "張信哲   別怕我傷心 無損音樂FLAC 歌詞LYRICS 純享",
+        "vid": "LNpXHqxw-Jw",
+        "surname": "張"
+    },
+    {
+        "id": 31,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 錯過你錯過愛 ] 官方完整版 MV (feat.于台煙)",
+        "vid": "8Fl2HhOKIKA",
+        "surname": "張"
+    },
+    {
+        "id": 32,
+        "artist": "張信哲",
+        "title": "【纯享】张信哲＆张杰《别怕我伤心》温婉凄美的旋律，配上忧郁的嗓音，深深传递出无奈与哀伤的情绪｜声生不息宝岛季 EP6 Infinity and Beyond 2023 | MangoTV",
+        "vid": "XKJh5zBVmYs",
+        "surname": "張"
+    },
+    {
+        "id": 33,
+        "artist": "張信哲",
+        "title": "【纯享】张信哲＆周兴哲《你不知道的事》两人歌声交织出新的情感，将诉不尽又道不明的苦情思揉尽歌曲中｜声生不息宝岛季 EP8 Infinity and Beyond 2023 | MangoTV",
+        "vid": "hfugNPTrfAM",
+        "surname": "張"
+    },
+    {
+        "id": 34,
+        "artist": "張信哲",
+        "title": "张信哲 《别怕我伤心》Don't be afraid of my sadness",
+        "vid": "K1JaJnn9xEA",
+        "surname": "張"
+    },
+    {
+        "id": 35,
+        "artist": "張信哲",
+        "title": "張信哲   過火 無損音樂FLAC 歌詞LYRICS 純享",
+        "vid": "ECuVZlkae-4",
+        "surname": "張"
+    },
+    {
+        "id": 36,
+        "artist": "張信哲",
+        "title": "张信哲 - 爱如潮水 【歌词版】",
+        "vid": "H1EsWG7_3Rk",
+        "surname": "張"
+    },
+    {
+        "id": 37,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 為靠近而遠離 ] 官方完整版 Official MV",
+        "vid": "9cymwy92ffM",
+        "surname": "張"
+    },
+    {
+        "id": 38,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 信仰 ] 官方完整版 MV",
+        "vid": "QoPxzsqGodk",
+        "surname": "張"
+    },
+    {
+        "id": 39,
+        "artist": "張信哲",
+        "title": "【纯享版】张信哲凭什么是情歌王子,听完《过火》你就明白了 | 华语金曲Best Chinese Music | SichuanTV【围炉音乐会】",
+        "vid": "P0zy202OqLU",
+        "surname": "張"
+    },
+    {
+        "id": 40,
+        "artist": "張信哲",
+        "title": "張信哲 Jeff Chang [ 守候 ] 官方完整版 Official MV",
+        "vid": "S1I4AHlKDJQ",
+        "surname": "張"
+    },
+    {
+        "id": 41,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin - 偉大的渺小 Little Big Us (華納 Official HD 官方MV)",
+        "vid": "JwjBbWQs71k",
+        "surname": "林"
+    },
+    {
+        "id": 42,
+        "artist": "林俊傑",
+        "title": "林俊傑 - 修煉愛情 伴奏ktv",
+        "vid": "GI7wzhpwwLM",
+        "surname": "林"
+    },
+    {
+        "id": 43,
+        "artist": "林俊傑",
+        "title": "林俊杰 - 黑夜问白天(2017)🎙️伴奏🎙️KTV",
+        "vid": "J-tXGVTfq_s",
+        "surname": "林"
+    },
+    {
+        "id": 44,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin《交換餘生 No Turning Back》Official Music Video",
+        "vid": "mNQwhIx_0CM",
+        "surname": "林"
+    },
+    {
+        "id": 45,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin - 手心的薔薇 Beautiful feat. G.E.M. 鄧紫棋 (華納 Official 高畫質 HD 官方完整版 MV)",
+        "vid": "onYP5u0b3yw",
+        "surname": "林"
+    },
+    {
+        "id": 46,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin - 修煉愛情 Practice Love (華納official 高畫質HD官方完整版MV)",
+        "vid": "LWV-f6dMN3Q",
+        "surname": "林"
+    },
+    {
+        "id": 47,
+        "artist": "林俊傑",
+        "title": "林俊杰   她说【伴奏】",
+        "vid": "12Ra2Uhh4vc",
+        "surname": "林"
+    },
+    {
+        "id": 48,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin - 黑夜問白天 53‭ ‬Dawns (華納 Official HD 官方MV)",
+        "vid": "DrBQeUOdQ_Y",
+        "surname": "林"
+    },
+    {
+        "id": 49,
+        "artist": "林俊傑",
+        "title": "[Karaoke] 江南 - 林俊傑（伴奏版）",
+        "vid": "X3utks1oAfE",
+        "surname": "林"
+    },
+    {
+        "id": 50,
+        "artist": "林俊傑",
+        "title": "林俊杰《输了你赢了世界又如何》 轻轻松松飙高音 绝对不可错过的神级live现场！《梦想的声音2》EP4 | 纯享 ZJSTV",
+        "vid": "NiHF-cwto_A",
+        "surname": "林"
+    },
+    {
+        "id": 51,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin 《Like You Do》 Official Music Video",
+        "vid": "RqNamO22aHI",
+        "surname": "林"
+    },
+    {
+        "id": 52,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin – 黑鍵 Black Keys (華納 Official 高畫質 HD 官方完整版 MV)",
+        "vid": "MgjdopPe79c",
+        "surname": "林"
+    },
+    {
+        "id": 53,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin  /  周深 Charlie Zhou Shen -《裹着心的光》 Light of Sanctuary - JJ20 現場版 Live in Beijing",
+        "vid": "iJL7wBl2QqY",
+        "surname": "林"
+    },
+    {
+        "id": 54,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin - 《不流淚的機場》 Flying on Your Wings - JJ20 重慶站現場版 Live in Chongqing",
+        "vid": "4Nl3YVy5fvw",
+        "surname": "林"
+    },
+    {
+        "id": 55,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin -《崇拜》 j’Adore - JJ20 現場版 Live in Wuhan",
+        "vid": "mxOczpo0fN8",
+        "surname": "林"
+    },
+    {
+        "id": 56,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin /   韋禮安 WeiBird 《如果可以》 Red Scarf - JJ20 FINAL LAP 台北現場版 Live in Taipei",
+        "vid": "D-_kqJX5HC0",
+        "surname": "林"
+    },
+    {
+        "id": 57,
+        "artist": "林俊傑",
+        "title": "林俊傑-孤獨娛樂 歌詞",
+        "vid": "QreCZ_fPBuM",
+        "surname": "林"
+    },
+    {
+        "id": 58,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin /  陶喆 @DavidTaoOfficial -《愛我還是他》Who Do You Love? - JJ20 重慶站現場版 Live in Chongqing",
+        "vid": "WGVE2bi4viE",
+        "surname": "林"
+    },
+    {
+        "id": 59,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin 《孤獨娛樂 Happily, Painfully After》Official Music Video",
+        "vid": "GSQV3oXyjMQ",
+        "surname": "林"
+    },
+    {
+        "id": 60,
+        "artist": "林俊傑",
+        "title": "林俊傑 JJ Lin / 胡彥斌 Tiger Hu -《男人KTV》 Karaoke Men - JJ20 現場版 Live in Suzhou",
+        "vid": "4UJqoKAFpqs",
+        "surname": "林"
+    },
+    {
+        "id": 61,
+        "artist": "蔡依林",
+        "title": "R3HAB & 蔡依林 Jolin Tsai《Stars Align》Official Music Video",
+        "vid": "--giQqhWGdQ",
+        "surname": "蔡"
+    },
+    {
+        "id": 62,
+        "artist": "蔡依林",
+        "title": "JOLIN 蔡依林《Pleasure》Official Music Video",
+        "vid": "j1TTQREaZMg",
+        "surname": "蔡"
+    },
+    {
+        "id": 63,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai - I'm Not Yours Feat. 安室奈美惠 NAMIE AMURO",
+        "vid": "C7wRb9adQUc",
+        "surname": "蔡"
+    },
+    {
+        "id": 64,
+        "artist": "蔡依林",
+        "title": "蔡依林 大丈夫 華納official HQ官方版MV",
+        "vid": "KoipVUiX-Gs",
+        "surname": "蔡"
+    },
+    {
+        "id": 65,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai - PLAY我呸 (華納official 高畫質HD官方完整版MV)",
+        "vid": "c86t8hoVw8E",
+        "surname": "蔡"
+    },
+    {
+        "id": 66,
+        "artist": "蔡依林",
+        "title": "說愛你 (伴奏)蔡依林",
+        "vid": "hwXgZ8dx8yE",
+        "surname": "蔡"
+    },
+    {
+        "id": 67,
+        "artist": "蔡依林",
+        "title": "JOLIN 蔡依林《DIY》Official Music Video",
+        "vid": "lyuiNKBKyug",
+        "surname": "蔡"
+    },
+    {
+        "id": 68,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《Mr.Q》(feat.徐熙娣 小S) Official Live MV",
+        "vid": "zvfIgf9oa_0",
+        "surname": "蔡"
+    },
+    {
+        "id": 69,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《玫瑰少年 Womxnly》Official Dance Video",
+        "vid": "feOq6MWeUXA",
+        "surname": "蔡"
+    },
+    {
+        "id": 70,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《說愛你》(feat.G.E.M.鄧紫棋) Official Live Music Video",
+        "vid": "Aq6KNRyvm-Y",
+        "surname": "蔡"
+    },
+    {
+        "id": 71,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai - 第三人稱 The Third Person And I (華納official 高畫質HD官方完整版MV)",
+        "vid": "2BrzK4fZ45k",
+        "surname": "蔡"
+    },
+    {
+        "id": 72,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai - 大藝術家The Great Artist (華納official 高畫質HD官方完整版MV)",
+        "vid": "bWx-vtCSg0w",
+        "surname": "蔡"
+    },
+    {
+        "id": 73,
+        "artist": "蔡依林",
+        "title": "JOLIN 蔡依林《Pillow》Official Music Video",
+        "vid": "kTHdB7BQ8wE",
+        "surname": "蔡"
+    },
+    {
+        "id": 74,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《怪美的 UGLY BEAUTY》Official Music Video",
+        "vid": "-wIHmPAvMBo",
+        "surname": "蔡"
+    },
+    {
+        "id": 75,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《OH LA LA LA（麥當勞40週年主題曲）》Official Music Video",
+        "vid": "AHDu0hGzuDo",
+        "surname": "蔡"
+    },
+    {
+        "id": 76,
+        "artist": "蔡依林",
+        "title": "蔡依林 - 倒帶(KTV伴唱)",
+        "vid": "Mu2Qrzf73Fg",
+        "surname": "蔡"
+    },
+    {
+        "id": 77,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《腦公 Hubby》Official Music Video",
+        "vid": "UYWUAQhhR_g",
+        "surname": "蔡"
+    },
+    {
+        "id": 78,
+        "artist": "蔡依林",
+        "title": "蔡依林 Jolin Tsai《心引力》 (feat.王俊凱) Official Live Music Video",
+        "vid": "iKf5UJG1SSU",
+        "surname": "蔡"
+    },
+    {
+        "id": 79,
+        "artist": "蔡依林",
+        "title": "蔡依林 倒带 -  Pinyin Karaoke Version - 拼音卡拉OK伴奏 - KTV with Pinyin Lyrics",
+        "vid": "n98xjFtDsZs",
+        "surname": "蔡"
+    },
+    {
+        "id": 80,
+        "artist": "蔡依林",
+        "title": "JOLIN 蔡依林《DIY》Dance Practice Video",
+        "vid": "dmn6OBmdfds",
+        "surname": "蔡"
+    }
+];
