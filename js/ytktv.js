1192ab8cf95499517f2b95da1b958d4af5f9d9fe
James <james.peng.pkj@gmail.com>
2026-01-02 11:56:23 +0800

    make it better

---
+++ js/ytktv.js
@@ -0,0 +1,92 @@
+const songDB = [
+    {
+        "id": 1,
+        "artist": "周杰倫",
+        "title": "【KTV】周杰伦 Jay Chou《安静》原版伴奏 | 高清歌词 (Karaoke Version)",
+        "vid": "zfO7_kLfudI",
+        "quality": "HD",
+        "channel": "Kaizy TV",
+        "surname": "周"
+    },
+    {
+        "id": 2,
+        "artist": "周杰倫",
+        "title": "純音樂 周杰倫 Jay Chou ─《不能說的秘密》Wild West KTV 伴唱 Karaoke 伴奏 西野",
+        "vid": "YGceKnbXWzQ",
+        "quality": "HD",
+        "channel": "西野Wild West KTV",
+        "surname": "周"
+    },
+    {
+        "id": 3,
+        "artist": "周杰倫",
+        "title": "經典 周杰倫  軌跡 KTV伴奏",
+        "vid": "UzexwBFshsg",
+        "quality": "HD",
+        "channel": "Micmeal Johnson",
+        "surname": "周"
+    },
+    {
+        "id": 4,
+        "artist": "周杰倫",
+        "title": "純音樂 周杰倫  ─《愛你沒差》Wild West KTV 伴唱 Karaoke",
+        "vid": "bydMr5IzrrQ",
+        "quality": "HD",
+        "channel": "西野Wild West KTV",
+        "surname": "周"
+    },
+    {
+        "id": 5,
+        "artist": "周杰倫",
+        "title": "[KARAOKE] Jay Chou 周杰倫【稻香 Rice Field】",
+        "vid": "W3RY-E8oiYc",
+        "quality": "HD",
+        "channel": "JOHANNA VONG",
+        "surname": "周"
+    },
+    {
+        "id": 6,
+        "artist": "周杰倫",
+        "title": "（伴奏版）明明就 - 周杰伦 《歌词拼音》【明明就不习惯牵手　为何却主动把手勾】",
+        "vid": "I4Ycr3YATkA",
+        "quality": "HD",
+        "channel": "music soul",
+        "surname": "周"
+    },
+    {
+        "id": 7,
+        "artist": "周杰倫",
+        "title": "【繁中】周杰倫-等你下課KTV(無人聲)",
+        "vid": "YL8Z78av-Bs",
+        "quality": "HD",
+        "channel": "大西國中-劉梓華",
+        "surname": "周"
+    },
+    {
+        "id": 8,
+        "artist": "周杰倫",
+        "title": "周杰伦   发如雪 Karaoke",
+        "vid": "CsQBQB08JS4",
+        "quality": "HD",
+        "channel": "Guixiang Xu",
+        "surname": "周"
+    },
+    {
+        "id": 9,
+        "artist": "周杰倫",
+        "title": "【KTV】周杰伦 Jay Chou《简单爱》原版伴奏 | 高清歌词 (Karaoke Version)",
+        "vid": "wJt-B0hIcmg",
+        "quality": "HD",
+        "channel": "Kaizy TV",
+        "surname": "周"
+    },
+    {
+        "id": 10,
+        "artist": "周杰倫",
+        "title": "（伴奏版）我不配 - 周杰伦 《歌词拼音》【这感觉已经不对 我努力在挽回】",
+        "vid": "SUZXHb7VVIc",
+        "quality": "HD",
+        "channel": "music soul",
+        "surname": "周"
+    }
+];
